85d959993a3dfaa20c4b45a0ffe0062c560add38
BT_1N3_20 <sample@bths.edu>
2025-01-10 10:49:04 -0500

    simultaneous, same line in same file

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("yeah");
-        System.out.println("something random");
+        System.out.println("MM");
         System.out.println("mark changed this line");
 
 
