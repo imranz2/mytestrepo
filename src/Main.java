4a0003c296210665904269cc03d0c31d381fe947
BT_1N3_20 <sample@bths.edu>
2025-01-10 10:44:23 -0500

    simultaneous commit print

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("filipino drake");
         System.out.println("something random");
+        System.out.println("simultaneous");
+
+
     }
 }
